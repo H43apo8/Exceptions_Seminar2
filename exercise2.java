3ff261246b19a738c46fa03edbdf2b3530ebe5f9
H43apo8 <118731185+H43apo8@users.noreply.github.com>
2023-08-05 11:23:54 +0300

    Update exercise2.java

--- exercise2.java
+++ exercise2.java
@@ -1,17 +1,19 @@
-/*
-Если необходимо, исправьте данный код.
-Решение: создан отсутствющий массив.
-*/
-public class program2 {
+public class ExceptionHandlingExample {
     public static void main(String[] args) {
+        int[] intArray = {1, 2, 3, 4, 5, 6, 7, 8};
+
         try {
-            int[] intArray = {3,5,6,7,8,5,4,5,6,7,7};
             int d = 0;
-            double catchedRes1 = intArray[8] / d;
+            double catchedRes1 = intArray[8] / d; // Попытка деления на ноль
             System.out.println("catchedRes1 = " + catchedRes1);
         } catch (ArithmeticException e) {
+            // Обработка исключения типа ArithmeticException
             System.out.println("Catching exception: " + e);
+        } finally {
+            // Блок finally выполняется независимо от того, было ли исключение или нет
+            System.out.println("Finally block is executed");
         }
 
+        System.out.println("After try-catch block");
     }
 }
